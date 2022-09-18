9f70d08a402af2c41569461b4dc2b99dc08ea6ae
Buxanka <ksu14102@yandex.ru>
2022-09-18 14:09:40 +0500

    Добавьте файлы проекта.

---
+++ Class.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using TextCopy;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateLab
+{
+    class Journal
+    {
+        public static string NameOfStudent = "John Dow", Discipline = "Discipline";
+        public static byte Mark = 0;
+        public static DateTime DateOfMark = DateTime.Now;
+        private static string path = "Journal.txt";
+
+        public void SetName(string _name)
+        {
+            NameOfStudent = _name;
+        }
+        public string GetName()
+        {
+            return NameOfStudent;
+        }
+        public void PrintName()
+        {
+            Console.WriteLine(NameOfStudent);
+        }
+        public void WriteToFile()
+        {
+            string myDocs = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            path = myDocs + "\\" + path;
+            using (StreamWriter sw = File.AppendText(path))
+                sw.WriteLine(NameOfStudent + " - " + Discipline);
+            Console.WriteLine("Информация добавлена в файл {0}.", path);
+        }
+        public void CopyToClipboard()
+        {
+            ClipboardService.SetText(NameOfStudent + " " + Discipline);
+            Console.WriteLine("Информация добавлена в буфер обмена.");
+        }
+        public string GetFromClipboard()
+        {
+            return ClipboardService.GetText();
+        }
+        public void SetDiscipline(string _discipline)
+        {
+            Discipline = _discipline;
+        }
+        public string GetDiscipline()
+        {
+            return Discipline;
+        }
+        public void PrintDiscipline()
+        {
+            Console.WriteLine(Discipline);
+        }
+        public void PrintNameAndDiscipline()
+        {
+            Console.WriteLine(NameOfStudent + " - " + Discipline);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateLab
+{
+    class Program
+    {
+        delegate void Output();
+
+        static void Choice(Journal myJournal)
+        {
+            Output _myOutput;
+            Console.WriteLine("Вывести на экран? (Y) На экран и в файл (F)");
+            string answer = Console.ReadLine();
+            if (answer == "Y" | answer == "y")
+            {
+                _myOutput = myJournal.PrintNameAndDiscipline;
+            }
+            else
+            {
+                if (answer == "F" | answer == "f")
+                {
+                    _myOutput = myJournal.PrintNameAndDiscipline;
+                    _myOutput += myJournal.WriteToFile;
+                }
+                else
+                {
+                    _myOutput = myJournal.WriteToFile;
+                }
+
+            }
+            _myOutput += myJournal.CopyToClipboard;
+            _myOutput += () => Console.WriteLine("Hello");
+            _myOutput -= myJournal.PrintName;
+            _myOutput += myJournal.PrintName;
+            _myOutput();
+        }
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите имя студента: ");
+            var myJournal = new Journal();
+            myJournal.SetName(Console.ReadLine());
+            Console.WriteLine("Введите дисциплину: ");
+            myJournal.SetDiscipline(Console.ReadLine());
+            Choice(myJournal);
+            Console.WriteLine(myJournal.GetFromClipboard());
+            Output func = () =>
+            {
+                Console.WriteLine("Hello");
+                Console.WriteLine("Hello".Length);
+            };
+            func();
+            //var func001(string _text) => Console.WriteLine(_text);
+
+        }
+    }
+}
